6e373dca2e20d5a07900ac69c615a2268cca0080
mkroxx <56604567+mkroxx@users.noreply.github.com>
2020-09-22 13:59:43 +0530

    Update ViewController.java
    Updated by Mukund

--- FinancialGoals/src/main/java/com/mukund/demo/ViewController.java
+++ FinancialGoals/src/main/java/com/mukund/demo/ViewController.java
@@ -13,19 +13,25 @@ import org.springframework.web.servlet.ModelAndView;
 public class ViewController {
 	
 	@Autowired
-	GoalRepo repo;
+	private GoalRepo repo;
 	@Autowired
-	TransactionRepo trepo;
+	private TransactionRepo trepo;
 	private Goal g;
-	
+	private int id;
 	@RequestMapping("view")
-	public ModelAndView getHomepage(int id ,String status) {
+	public ModelAndView getHomepage(String gid ,String status) {
 		ModelAndView mv = new ModelAndView();
-		g=repo.findById(id).get();
-		mv.addObject("goal",g);
-		mv.addObject("transaction",getByGoalId(id,trepo.findAll()));
-		mv.addObject("r",status);
-		mv.setViewName("View.jsp");
+		try {
+			id=Integer.parseInt(gid);
+			g=repo.findById(id).get();
+			mv.addObject("goal",g);
+			mv.addObject("transaction",getByGoalId(id,trepo.findAll()));
+			mv.addObject("r",status);
+			mv.setViewName("View.jsp");
+		}
+		catch(Exception e) {
+			mv.setViewName("home");
+		}
 		return mv;
 	}
 	
@@ -51,15 +57,18 @@ public class ViewController {
 			repo.save(g);
 			mv.addObject("status","Transaction Successful.");
 			}
+			else if(g.getContribute()==g.getTargetAmount()) {
+				mv.addObject("status","Transaction Unsuccessful. Goal is already Completed.");
+			}
 			else {
-				mv.addObject("status","Transaction Unsuccessful. Required Amount="+(g.getTargetAmount()-g.getContribute()));
+				mv.addObject("status","Transaction Unsuccessful.\nContributionAmount <= RequiredAmount.");
 			}
 		}
 		catch (Exception e) {
 			// TODO: handle exception
 			mv.addObject("status",e.getMessage());
 		}
-		mv.addObject("id",g.getId());
+		mv.addObject("gid",g.getId());
 		mv.setViewName("redirect:view");
 		return mv;
 		
